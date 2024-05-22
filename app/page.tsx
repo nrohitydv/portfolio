170539f63b063c418149b27264ef6f71ffa2a7c6
Rohit <rohityadav787898@gmail.com>
2024-05-22 19:42:07 +0545

    Addded magicbutton,textgenerateeffect and spotlight

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex items-center justify-center flex-col overflow-hidden mx-auto sm:px-10 px-10 ">
       <div className="max-w-7xl w-full">
-        <h1>Hello world!</h1>
+        <Hero />
       </div>
     </main>
   );
