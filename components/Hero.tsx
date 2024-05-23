7f04070d19174a900ce3647ad877522aadb43329
Rohit <rohityadav787898@gmail.com>
2024-05-23 14:18:24 +0545

    "Updated components and dependencies: added FloatingNav, Grid, and react-icons; modified Hero and MagicButton; updated package.json and package-lock.json; replaced next.svg and removed vercel.svg"

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import MagicButton from "./ui/MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -26,13 +27,17 @@ const Hero = () => {
           </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless Experiences"
+            words="Transforming Concepts into Seamless User Experiences"
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Rohit, a Next.js Developer base in Nepal.
           </p>
           <a href="#about">
-            <MagicButton title="Show my Work " />
+            <MagicButton
+              title="Show my Work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
           </a>
         </div>
       </div>
