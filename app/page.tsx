9f790fe45b590d40ea10300d6cba8300b257333c
Rohit <rohityadav787898@gmail.com>
2024-05-24 13:48:48 +0545

    "Refactor navigation components: updated nav items, removed login button, and adjusted styles in FloatingNav and RecentProjects components."

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,13 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Grid from "@/components/ui/Grid";
-import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex items-center justify-center flex-col overflow-hidden mx-auto sm:px-10 px-10 ">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
